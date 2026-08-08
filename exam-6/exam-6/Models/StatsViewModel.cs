81fb787e088b6751e80e831a03d7c1f2cf29f5d5
Kylychbekov Nursultan <nursultankylychbekov05@gmail.com>
2026-08-08 13:55:03 +0600

    Уровень 1: коммит модели задач

---
+++ exam-6/exam-6/Models/StatsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApp;
+
+public class StatsViewModel
+{
+    public int TotalTasks { get; set; }
+    public int DoneTasks { get; set; }
+    public int NewTasks { get; set; }
+    public int DonePercent { get; set; }
+    public int NewPercent { get; set; }
+}
---
+++ exam-6/exam-6/Models/TodoTask.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoApp;
+
+public class TodoTask
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public string Assignee { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string CreatedAt { get; set; } = DateTime.Now.ToString("dd.MM.yyyy");
+    public string? CompletedAt { get; set; } = "-";
+    public string Status { get; set; } = "new"; 
+}
