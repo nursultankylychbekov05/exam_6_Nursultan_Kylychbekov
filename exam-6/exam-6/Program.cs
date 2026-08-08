1d6d68d9f999546b430a1f335965e2e72aebdc5c
Kylychbekov Nursultan <nursultankylychbekov05@gmail.com>
2026-08-08 17:41:01 +0600

    Финальный коммит (Надеюсь)

--- exam-6/exam-6/Program.cs
+++ exam-6/exam-6/Program.cs
@@ -5,6 +5,6 @@ class Program
     static async Task Main(string[] args)
     {
         var server = new DumbHttpServer();
-        await server.RunAsync("../../../site", 8000);
+        await server.RunAsync("../../../site", 8888);
     }
 }
